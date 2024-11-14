fa248d5c1f47c093242eb38b36597e571ab19ccb
kashafchh <kashafch26@gmail.com>
2024-11-14 14:43:12 +0500

    Add files via upload

---
+++ number.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo " enter a number:"
+read num
+if [ "$num" -eq 1 ] ; then
+echo " you pressed 1."
+elif [ "$num" -eq 1 ] ; then
+echo " you pressed 1."
+elif [ "$num" -eq 2 ] ; then
+echo " you pressed 2."
+elif [ "$num" -eq 3 ] ; then
+echo " you pressed 3."
+elif [ "$num" -eq 4 ] ; then
+echo " you pressed 4."
+elif [ "$num" -eq 5 ] ; then
+echo " you pressed 5."
+
+else
+echo " invalid number."
+fi
+
+
+
